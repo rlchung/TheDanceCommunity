9c8119df939e3b537974da22781388b5ed2a0a36
Richard L Chung <richardchung93@gmail.com>
2016-07-01 05:55:19 +0000

    revised token

--- team_methods.js
+++ team_methods.js
@@ -2,13 +2,17 @@ var mongoose    = require("mongoose"),
     request     = require("request"),
     Event       = require("./models/event"),
     Team        = require("./models/team");
+    
+var secret = "2a3058e3435b71c77c50bec7302dcff8",
+    app_id = "263412864027390",
+    token = app_id + '|' + secret;
 
-// function teamTest(){ 
-//     request('https://graph.facebook.com/545398148962340?fields=events.limit(1)&access_token=263412864027390|2a3058e3435b71c77c50bec7302dcff8', function (error, response, body) {
-//         if (!error && response.statusCode == 200) {
-//             console.log(body) // Show the HTML for the Google homepage. 
-//         }
-//     });
-// }
+function teamTest(){ 
+    request('https://graph.facebook.com/545398148962340?fields=events.limit(1)&access_token=' + token, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body) // Show the HTML for the Google homepage. 
+        }
+    });
+}
 
 module.exports = teamTest;

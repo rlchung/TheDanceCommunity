16c05fc8a753c73676cb4eb8cdf79d42c5f51320
Richard L Chung <richardchung93@gmail.com>
2016-07-01 07:46:30 +0000

    sample logic for initialize_Team function in teamTest

--- team_methods.js
+++ team_methods.js
@@ -2,17 +2,31 @@ var mongoose    = require("mongoose"),
     request     = require("request"),
     Event       = require("./models/event"),
     Team        = require("./models/team");
-    
+
+//CONFIDENTIAL APP TOKENS 
 var secret = "2a3058e3435b71c77c50bec7302dcff8",
     app_id = "263412864027390",
     token = app_id + '|' + secret;
 
 function teamTest(){ 
-    request('https://graph.facebook.com/545398148962340?fields=events.limit(1)&access_token=' + token, function (error, response, body) {
+    request('https://graph.facebook.com/samahangmodern?fields=name,id,bio&access_token=' + token, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log(body) // Show the HTML for the Google homepage. 
+            var body1 = JSON.parse(body);
+            request('https://graph.facebook.com/samahangmodern/photos?fields=images&access_token='+ token, function (error, response, body){
+                if (!error && response.statusCode == 200) {
+                    var body2 = JSON.parse(body);
+                    console.log(body1["name"]); //logs out the name of body1
+                    console.log(body2["data"][0]["images"][0]["source"]); //logs out the url of the full image
+                } 
+            });
         }
     });
 }
+
+// Creates a Team object with all necessary parameters (except events for now)
+// To be implemented: event population function 
+// function intialize_Team(){
+    
+// };
 
 module.exports = teamTest;

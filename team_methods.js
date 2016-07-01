1cae1e3b6d383878f65030bd77f8a273828d80d2
Richard L Chung <richardchung93@gmail.com>
2016-07-01 08:40:17 +0000

    refactored initialize_team code

--- team_methods.js
+++ team_methods.js
@@ -16,7 +16,9 @@ function teamTest(){
                 if (!error && response.statusCode == 200) {
                     var body2 = JSON.parse(body);
                     console.log(body1["name"]); //logs out the name of body1
+                    console.log(body1["id"]);
                     console.log(body2["data"][0]["images"][0]["source"]); //logs out the url of the full image
+                    console.log(body1["bio"]);
                 } 
             });
         }
@@ -25,8 +27,34 @@ function teamTest(){
 
 // Creates a Team object with all necessary parameters (except events for now)
 // To be implemented: event population function 
-// function intialize_Team(){
-    
-// };
+function intialize_Team(){
+    request('https://graph.facebook.com/samahangmodern?fields=name,id,bio&access_token=' + token, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var info_json = JSON.parse(body);
+            request('https://graph.facebook.com/samahangmodern/photos?fields=images&access_token='+ token, function (error, response, body){
+                if (!error && response.statusCode == 200) {
+                    var images_json = JSON.parse(body);
+                    
+                    var name    = info_json["name"],
+                        id      = info_json["id"],
+                        bio     = info_json["bio"],
+                        image   = images_json[0]["images"][0]["source"];
+                    
+                    var newTeam = {
+                        name : name,
+                        id : id,
+                        image : image,
+                        description : bio
+                    }
+                    
+                    console.log(name);
+                    console.log(id);
+                    console.log(image);
+                    console.log(bio);
+                } 
+            });
+        }
+    });
+};
 
 module.exports = teamTest;

b12f266004aba1ab86456150fef6e397c37889ed
Richard L Chung <richardchung93@gmail.com>
2016-11-30 07:51:38 +0000

    configured credentials for public repo

--- credentials.js
+++ credentials.js
@@ -1,10 +1,11 @@
-//CONFIDENTIAL APP TOKENS 
-var fb_secret = "2a3058e3435b71c77c50bec7302dcff8",
-    fb_app_id = "263412864027390",
+// CONFIDENTIAL APP TOKENS
+// NOTES: API keys and token are removed for confidentiality
+var fb_secret = "",
+    fb_app_id = "",
     fb_token = fb_app_id + '|' + fb_secret,
-    cloud_name = 'dce6r25eh',
-    cloud_api_key = '197979542757464',
-    cloud_api_secret = '-KoGLzFEy9SFF051ZP5virkgHkU';
+    cloud_name = '',
+    cloud_api_key = '',
+    cloud_api_secret = '';
     
 module.exports = {
     fb_secret           : fb_secret,
